9c59fd79883b50e92b2dbc9c16847930a05689ab
pbr369 <143585864+pbr369@users.noreply.github.com>
2023-11-07 12:57:28 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import "./App.css";
+import TicTacToe from "./components/TicTacToe";
+
+export default function App() {
+  return <TicTacToe />;
+}
---
+++ src/components/Strike.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Strike({ strikeClass }) {
+  return <div className={`strike ${strikeClass}`}></div>;
+}
